117c3b662ddf25f23eedf6b1ce034156ed97e882
Peter Perov <peterperov@gmail.com>
2024-03-19 19:00:47 +0000

    tuesday

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# DocumentIntelligence
+
+
+
+# Prereqs
+
+
+https://pypi.org/project/azure-ai-formrecognizer/
+
+
+```
+    pip install azure-ai-formrecognizer
+```
+
+
+# Codez
+
+```
+document_analysis_client.begin_analyze_document
+
+```
+
+https://learn.microsoft.com/en-us/python/api/azure-ai-formrecognizer/azure.ai.formrecognizer.documentanalysisclient?view=azure-python#azure-ai-formrecognizer-documentanalysisclient-begin-analyze-document
+
+```
+
+```
--- run-analysis.py
+++ run-analysis.py
@@ -9,23 +9,38 @@ https://learn.microsoft.com/azure/ai-services/document-intelligence/quickstarts/
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.formrecognizer import DocumentAnalysisClient
 
-"""
-Remember to remove the key from your code when you're done, and never post it publicly. For production, use
-secure methods to store and access your credentials. For more information, see 
-https://docs.microsoft.com/en-us/azure/cognitive-services/cognitive-services-security?tabs=command-line%2Ccsharp#environment-variables-and-application-configuration
-"""
-endpoint = "YOUR_FORM_RECOGNIZER_ENDPOINT"
-key = "YOUR_FORM_RECOGNIZER_KEY"
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+speech_api_key = config.get("AZURE_SPEECH_API_KEY", None)
+speech_region = config.get("AZURE_SPEECH_REGION", None)
+
+# FORM_RECOGNIZER_ENDPOINT
+fr_endpoint = config.get("FORM_RECOGNIZER_ENDPOINT", None)
+# FORM_RECOGNIZER_KEY
+fr_key = config.get("FORM_RECOGNIZER_KEY", None)
+
+image_file = "W:/GITHUB/DocumentIntelligence/IrishDL/Ireland DL 001.png"
+
+# W:/GITHUB/DocumentIntelligence/SamplePictures/Driving License 01.jpg
+image_file = "W:/GITHUB/DocumentIntelligence/SamplePictures/Driving License 01.jpg"
 
 # sample document
 formUrl = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/DriverLicense.png"
 
-document_analysis_client = DocumentAnalysisClient(
-        endpoint=endpoint, credential=AzureKeyCredential(key)
+document_analysis_client = DocumentAnalysisClient( endpoint=fr_endpoint, credential=AzureKeyCredential(fr_key) )
+# poller = document_analysis_client.begin_analyze_document_from_url("prebuilt-idDocument", formUrl)
+
+fr_locale="en-US"
+
+
+with open(image_file, "rb") as f:
+    poller = document_analysis_client.begin_analyze_document(
+        "prebuilt-idDocument", document=f, locale=fr_locale
     )
-    
-poller = document_analysis_client.begin_analyze_document_from_url("prebuilt-idDocument", formUrl)
 id_documents = poller.result()
+
+
 
 for idx, id_document in enumerate(id_documents.documents):
     print("--------Recognizing ID document #{}--------".format(idx + 1))
